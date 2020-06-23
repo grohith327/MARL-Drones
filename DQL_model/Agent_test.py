8650a6211de29133bb81e6990ada55ef5ffc2625
rohith <grohith327@gmail.com>
2020-06-22 23:15:11 -0400

    agent test

--- DQL_model/Agent_test.py
+++ DQL_model/Agent_test.py
@@ -9,31 +9,32 @@ import os
 
 
 class AgentModel(nn.Module):
-    def __init__(self, state_size, action_size):
+    def __init__(self, state_size, n_drones):
         super(AgentModel, self).__init__()
         self.state_input = nn.Linear(state_size, 64)
-        self.drone_input = nn.Linear(2, 64)
+        self.drone_input = nn.Linear(2 * n_drones, 64)
         self.dense1 = nn.Linear(128, 64)
-        self.dense2 = nn.Linear(64, 32)
-        self.dense3 = nn.Linear(32, action_size)
+        self.dense2 = nn.Linear(64, state_size)
 
     def forward(self, state, drone_pos):
         state = torch.tensor(state, dtype=torch.float)
+        drone_pos = drone_pos.flatten()
         drone_pos = torch.tensor(drone_pos, dtype=torch.float).unsqueeze(0)
         state_embed = F.leaky_relu(self.state_input(state))
         drone_embed = F.leaky_relu(self.drone_input(drone_pos))
         out = torch.cat([state_embed, drone_embed], dim=-1)
         out = F.leaky_relu(self.dense1(out))
-        out = F.leaky_relu(self.dense2(out))
-        out = self.dense3(out)
+        out = self.dense2(out)
         return out
 
 
 class Agent:
-    def __init__(self, state_size, action_size, n_drones):
+    def __init__(self, state_size, action_size, n_drones, row_count, col_count):
         self.state_size = state_size
         self.action_size = action_size
         self.n_drones = n_drones
+        self.row_count = row_count
+        self.col_count = col_count
         self.memory = deque(maxlen=2000)
         self.gamma = 0.95
         self.epsilon = 1.0
@@ -48,7 +49,7 @@ class Agent:
 
         self.mse_loss = nn.MSELoss()
         models = [
-            AgentModel(self.state_size, self.action_size) for _ in range(self.n_drones)
+            AgentModel(self.state_size, self.n_drones) for _ in range(self.n_drones)
         ]
         optimizers = [
             torch.optim.AdamW(models[i].parameters(), lr=self.learning_rate)
@@ -56,13 +57,13 @@ class Agent:
         ]
         return models, optimizers
 
-    def _fit(self, state, drone_pos, target, i):
-        self.models[i].zero_grad()
+    def _fit(self, model, optimizer, state, drone_pos, target):
+        model.zero_grad()
         target = torch.tensor(target, dtype=torch.float)
-        preds = self.models[i](state, drone_pos)
+        preds = model(state, drone_pos)
         loss = self.mse_loss(preds, target)
         loss.backward()
-        self.optimizers[i].step()
+        optimizer.step()
 
     def memorize(
         self, state, drone_pos, action, reward, next_state, next_drone_pos, done
@@ -71,21 +72,40 @@ class Agent:
             (state, drone_pos, action, reward, next_state, next_drone_pos, done)
         )
 
+    def pos_to_act(self, curr_x, curr_y, target):
+        target = target.reshape((self.row_count, self.col_count))
+        target_x, target_y = np.where(target == np.max(target))
+        target_x = target_x[0]
+        target_y = target_y[0]
+        if target_x - curr_x == 0 and target_y - curr_y == 0:
+            return 4
+        if target_y - curr_y < 0:
+            return 0
+        if target_y - curr_y > 0:
+            return 2
+        if target_x - curr_x > 0:
+            return 1
+        return 3
+
     def act(self, state, drone_pos, infer=False):
         actions = []
         rand_val = np.random.rand()
         for i in range(self.n_drones):
             if infer:
-                act_values = self.models[i](state, drone_pos[i])
+                act_values = self.models[i](state, drone_pos)
                 act_values = act_values.detach().numpy()
-                actions.append(np.argmax(act_values[0]))
+                actions.append(
+                    self.pos_to_act(drone_pos[i][0], drone_pos[i][1], act_values[0])
+                )
                 continue
             if rand_val <= self.epsilon:
                 actions.append(random.randrange(0, self.action_size - 1))
             else:
-                act_values = self.models[i](state, drone_pos[i])
+                act_values = self.models[i](state, drone_pos)
                 act_values = act_values.detach().numpy()
-                actions.append(np.argmax(act_values[0]))
+                actions.append(
+                    self.pos_to_act(drone_pos[i][0], drone_pos[i][1], act_values[0])
+                )
         return actions
 
     def replay(self, batch_size):
@@ -103,14 +123,14 @@ class Agent:
                 target = reward
                 if not done:
                     target = reward + self.gamma * np.amax(
-                        self.models[i](next_state, next_drone_pos[i])[0]
-                        .detach()
-                        .numpy()
+                        self.models[i](next_state, next_drone_pos)[0].detach().numpy()
                     )
-                target_f = self.models[i](state, drone_pos[i])
+                target_f = self.models[i](state, drone_pos)
                 target_f = target_f.detach().numpy()
                 target_f[0][action] = target
-                self._fit(state, drone_pos[i], target_f, i)
+                self._fit(
+                    self.models[i], self.optimizers[i], state, drone_pos, target_f
+                )
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
@@ -123,14 +143,16 @@ class Agent:
             self.models[i].load_state_dict(torch.load(f"{name}_drone_{i}.bin"))
 
 
-env = DroneEnv(row_count=5, col_count=5)
+env = DroneEnv()
 
 state_size = env.state_size
 action_size = env.action_size
 n_drones = env.n_drones
+row_count = env.row_count
+col_count = env.col_count
 
-agent = Agent(state_size, action_size, n_drones)
-agent.load("DQTweaks_torch")
+agent = Agent(state_size, action_size, n_drones, row_count, col_count)
+agent.load("DQL_envchanges")
 done = False
 
 state = env.reset()
@@ -141,7 +163,7 @@ drone_pos = drone_pos.reshape((n_drones, 2))
 i = 0
 print(f"Step: {i+1}")
 print(f"Drone positions:{env.n_drones_pos}")
-temp_st = state.reshape((5, 5))
+temp_st = state.reshape((10, 10))
 print(temp_st, "\n")
 
 """
@@ -195,7 +217,7 @@ while not done:
         print(f"Step: {i+1}")
         print(f"Drone positions:{env.n_drones_pos}")
         print(f"Action:{action}")
-        temp_st = state.reshape((5, 5))
+        temp_st = state.reshape((10, 10))
         print(temp_st, "\n")
 
     i += 1
